1e8ae47cd1337f1c272d60c1b5f1fa623f325ce7
Peter Cheung <peter@6uohk.com>
2022-04-12 01:22:06 +0100

    Lesson 53 -  Constructor Injection in Spring

---
+++ src/main/kotlin/com/test/kotlinspring/service/GreetingService.kt
@@ -0,0 +1,10 @@
+package com.test.kotlinspring.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class GreetingService {
+
+    fun retrieveGreeting(name: String) = "Hello $name"
+
+}
