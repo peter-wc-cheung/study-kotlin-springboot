bc53650395465ff53563d33982238958eec70ed9
Peter Cheung <peter@6uohk.com>
2022-10-21 00:19:57 +0100

    Spring JPA Test by ParameterizedTest and MethodSource

--- src/test/intg/com/test/kotlinspring/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/test/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -5,9 +5,13 @@ import mu.KLogging
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -20,6 +24,15 @@ class CourseRepositoryIntgTest {
         private const val ID = 100
         private const val NAME = "Build Restful APIs using SpringBoot and Kotlin"
         private const val CATEGORY = "Development"
+
+        // need to convert to static method in JAVA
+        @JvmStatic
+        private fun courseAndSize(): Stream<Arguments> {
+
+            return Stream.of(Arguments.arguments("SpringBoot", 2))
+
+        }
+
         private val logger = KLogging().logger
     }
 
@@ -46,6 +59,17 @@ class CourseRepositoryIntgTest {
         logger.info { "courses: ${courses}" }
 
         assertEquals(2, courses.size)
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+
+        val courses = courseRepository.findCoursesByName(name)
+        logger.info { "courses: ${courses}" }
+
+        assertEquals(expectedSize, courses.size)
 
     }
 
